b92c601ee8ff8704b5ad7ebd89b2d7e419b69f12
Tamias <n.tamia5@icloud.com>
2023-08-23 18:31:57 +0300

    update 01 and 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+const galleryItemsCardList = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+            <img class='gallery__image' src='${preview}' alt='${description}'>
+        </a>
+    </li>`)
+    .join('');
+console.log(galleryItemsCardList);
