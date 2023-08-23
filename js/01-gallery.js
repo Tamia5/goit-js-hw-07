077accee9ee85bcd463e3578ef6adad14b9bd099
Tamias <n.tamia5@icloud.com>
2023-08-23 18:09:24 +0300

    update 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+const galleryItemsCardList = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+            <img class='gallery__image'
+            src='${preview}' 
+            data-source='${original}' 
+            alt='${description}'>
+        </a>
+    </li>`)
+    .join('');
+
+// console.log(galleryItemsCardList);
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryItemsCardList);
+
+galleryListEl.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const largeImage = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src='${largeImage}'  width="800" height="600">`)
+    instance.show()
+
+    galleryListEl.addEventListener('keydown', event => {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    })
+});
